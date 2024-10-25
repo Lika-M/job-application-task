05ecaa781863621f559f752deeae1f244bd8de4d
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-10-25 11:05:39 +0300

    Made minor styling adjustments for improved UI.

--- src/components/QuestionnaireList.jsx
+++ src/components/QuestionnaireList.jsx
@@ -2,15 +2,15 @@
 import QuestionnaireItem from './QuestionnaireItem';
 
 const QuestionnaireList = ({ questionnaires, selectQuestionnaireForEdit, deleteQuestionnaire }) => {
-    console.log(questionnaires)
+    
     return (
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 py-8">
             {questionnaires.map((questionnaire) => (
                 <QuestionnaireItem
                     key={questionnaire.id}
                     questionnaire={questionnaire}
-                    // onSelect={selectQuestionnaireForEdit}
-                    // onDelete={deleteQuestionnaire}
+                    onSelect={selectQuestionnaireForEdit}
+                    onDelete={deleteQuestionnaire}
                 />
             ))}
         </div>
