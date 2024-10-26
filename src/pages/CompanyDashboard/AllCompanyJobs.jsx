219187117377e25c81b1df72ae8a70a0334856ea
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-10-26 12:30:45 +0300

    Updated job listing to handle empty array case with a message when no jobs are available.

--- src/pages/CompanyDashboard/AllCompanyJobs.jsx
+++ src/pages/CompanyDashboard/AllCompanyJobs.jsx
@@ -33,26 +33,30 @@ const AllCompanyJobs = () => {
 
     return (
         <section className="container mx-auto my-12 p-6 bg-white shadow-lg rounded-lg text-center">
-        <h1 className="text-4xl font-bold mb-8">Информация за Компанията</h1>
+            <h1 className="text-4xl font-bold mb-8">Информация за Компанията</h1>
 
-        <h1 className="text-4xl font-bold mb-8">Всички обяви на компанията на Компанията</h1>
-       
-        <article className="container mx-auto py-8">
-            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 py-8">
-                {jobs.map((job) => (
-                    <CompanyJobItem
-                        key={job.id}
-                        job={job}
-                        onSelect={handleSelectJobForEdit}
-                        onDelete={handleDeleteJob}
-                    />
-                ))}
-            </div>
-            <div>
-                <a href="/company-dashboard" className="inline-block mt-6 px-8 py-4 bg-[#004AAD] text-white font-semibold rounded hover:bg-blue-700">Обратно в Профила</a>
-            </div>
-        </article>
-    </section>
+            <h1 className="text-4xl font-bold mb-8">Всички обяви на компанията на Компанията</h1>
+
+            <article className="container mx-auto py-8">
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 py-8">
+                    {jobs.length > 0 ? (
+                        jobs.map((job) => (
+                            <CompanyJobItem
+                                key={job.id}
+                                job={job}
+                                onSelect={handleSelectJobForEdit}
+                                onDelete={handleDeleteJob}
+                            />
+                        ))
+                    ) : (
+                        <p className="text-gray-600">Все още нямате създадени обяви.</p> 
+                    )}
+                </div>
+                <div>
+                    <a href="/company-dashboard" className="inline-block mt-6 px-8 py-4 bg-[#004AAD] text-white font-semibold rounded hover:bg-blue-700">Обратно в Профила</a>
+                </div>
+            </article>
+        </section>
     );
 };
 
